547ffbdfcb0739ae8fa04b02e07f4ab09d63d129
anirudhramanan <anirudh.r@flipkart.com>
2020-05-25 12:21:07 +0530

    additional unit tests

---
+++ madman/src/test/java/com/flipkart/madman/provider/ProgressTest.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2020 Flipkart Internet Pvt Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.flipkart.madman.provider
+
+import org.junit.Test
+
+class ProgressTest {
+    @Test
+    fun testProgressEquality() {
+        val progress = Progress(1000, 10000)
+        val progress1 = Progress(1000, 10000)
+        val progress2 = Progress(100, 10000)
+
+        /** asset progress is equal **/
+        assert(progress == progress1)
+
+        /** asset progress is not equal **/
+        assert(progress != progress2)
+    }
+}
